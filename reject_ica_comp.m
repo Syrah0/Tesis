c168eb2f95af4cdd74a8f943bdefa0072b48dd3a
Alexandra Ibarra <alexasofia@hotmail.com>
2022-03-26 03:13:46 -0300

    remove irrelevant lines

--- reject_ica_comp.m
+++ reject_ica_comp.m
@@ -1,6 +1,6 @@
 %% cargar data
-path        = 'E:\DatosPsiquiatrico\Procesados\RS3\';
-file        = filenames{128};
+path        = '...';
+file        = '...';
 data        = load([path,file]);
 comp        = data.comp;
 
@@ -16,14 +16,13 @@ ft_topoplotIC(cfg, comp);% FT_TOPOPLOTIC plots the topographic distribution of a
 
 %component that was computed using the FT_COMPONENTANALYSIS function
 
-%% Señal con componentes
+%% SeÃ±al con componentes
 
 cfg            = [];
 cfg.viewmode   = 'component';
 cfg.layout     = 'biosemi64.lay'; % specify the layout file that should be used for plotting
 cfg.continuous = 'no';
 cfg.channel    = 1:30;
-% cfg.compscale  = 'global'; %'local'
 
 ft_databrowser(cfg,comp);
 sprintf('%s', file)

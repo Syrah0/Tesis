7e0b0e23ba3e4edb583ad0032b1043e90533dd2f
Alexandra Ibarra <alexasofia@hotmail.com>
2022-03-26 03:13:15 -0300

    remove irrelevant lines

--- rechazo_comp_1.m
+++ rechazo_comp_1.m
@@ -1,39 +1,25 @@
 %% RECHAZO ARTEFACTOS (databrowser)
-%%%% problema!!!! D:
 
 %% cargar data
-path    = 'E:\DatosPsiquiatrico\Procesados\STB\';
-file    = 'FEP_018_FIRST_STEP.mat';
+path    = '...';
+file    = '...';
 data    = load([path,file]);
 dataFIC = data.DATAEEG;
 
 %% Seleccionar artefactos manualmente
 
-% first select only the EEG channels
-% cfg         = [];
-% cfg.channel = 'EEG';
-% data        = ft_preprocessing(cfg,dataFIC);
-
 % open the browser and page through the trials
 cfg              = [];
-% cfg.channel      = 'EEG';
 cfg.viewmode     = 'vertical';
 cfg.continuous   = 'no';
 cfg.allowoverlap = 'yes';
 ft_databrowser(cfg,dataFIC);
-% cfg              = ft_databrowser(cfg,dataFIC);
-
-% ft_preprocessing(cfg, dataFIC);
-% ft_rejectartifact(cfg, dataFIC);
-
-% save([path, file(1:end-15), '_REJECT_COMP.mat'],'dataFIC') ; % POST
-% REJECT
 
 cfg            = [];
 cfg.method     = 'trial';
 cfg.viewmode   = 'summary' ;
 
-REJECT_COMP    = ft_rejectvisual(cfg,dataFIC); %Data EEG es mi dato con la señal
+REJECT_COMP    = ft_rejectvisual(cfg,dataFIC); %Data EEG es mi dato con la seÃ±al
 
 cfg            = [];
 cfg.continuous = 'no';
@@ -41,5 +27,3 @@ cfg.viewmode   = 'vertical';
 cfg.allowoverlap = 'yes';
 
 ft_databrowser(cfg, REJECT_COMP);%Dato con rechazo
-
-% save([path, file(1:end-15), '_REJECT_COMP.mat'],'REJECT_COMP'); % POST

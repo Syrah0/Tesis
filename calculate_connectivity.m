16a60a9f1a7ede94e033d9fededb729b2b31ef2b
Alexandra Ibarra <alexasofia@hotmail.com>
2022-03-26 03:07:53 -0300

    remove irrelevant lines

--- calculate_connectivity.m
+++ calculate_connectivity.m
@@ -1,9 +1,8 @@
 %%%%%% calculo de conectividad
 
 %% cargar data preprocesada
-path = 'E:\DatosPsiquiatrico\Procesados\STB\';
-correct_path = 'E:\DatosPsiquiatrico\Procesados\DatosCorrectos\STB\';
-%file = 'CNTF_007_DBF_R1__FINISH.mat';
+path = '...';
+correct_path = '...'; # where save new data
 file_list = dir([path, '*_FINISH.mat']);
 filenames = cell(1,length(file_list));
 
@@ -11,7 +10,7 @@ for i=1:length(file_list)
     filenames{i-0} = file_list(i).name;       
 end
 
-ids = [91 92 101 102];
+ids = []; # event types
 
 for j=1:length(file_list)
     file = filenames{j};
@@ -29,47 +28,17 @@ for j=1:length(file_list)
 
         data           = ft_preprocessing(cfg, data);
         
-        %% hacer analisis de time-frecuency
-        %% Computation of the multivariate autoregressive model
-
-        % cfg          = [];
-        % cfg.order    = 5;
-        % cfg.toolbox  = 'bsmart'; %%bsmart, biosig
-        % mdata        = ft_mvaranalysis(cfg, data);
-
-        %% Computation of the spectral transfer function
-
-        % cfg         = [];
-        % cfg.method  = 'mvar';
-        % mfreq       = ft_freqanalysis(cfg, mdata);
-
-        %% OR Non-parametric computation of the cross-spectral density matrix (VER BN)
-
-        % Some connectivity metrics can be computed from a non-parametric spectral 
-        % estimate (i.e. after the application of the FFT-algorithm and conjugate 
-        % multiplication to get cross-spectral densities), such as coherence, 
-        % phase-locking value and phase slope index. The following part computes the 
-        % Fourier-representation of the data using ft_freqanalysis.
-
-        % cfg           = [];
         cfg.method    = 'mtmfft';
         cfg.taper     = 'dpss';
         cfg.output    = 'fourier';
         cfg.tapsmofrq = 2;
         freq          = ft_freqanalysis(cfg, data);
 
-%         %% correccion de linea base --> el promedio lo corrigen
-%         cfg              = [];
-%         cfg.baseline     = [-0.3 -0.1]; % no usar linea base en el cero
-%         cfg.baselinetype = 'relchange';
-%         freq_blc         = ft_freqbaseline(cfg, freq);
-
         %% Computation and inspection of the connectivity measures
 
         cfg           = [];
         cfg.method    = 'coh';
         coh           = ft_connectivityanalysis(cfg, freq); %% muestra la conectividad de un electrodo con otro para una cierta frecuencia, la diagonal es 1 => 100% conectividad, mientras mas cerca de 1 => mas conectado estas
-        % cohm          = ft_connectivityanalysis(cfg, mfreq);
         
         coh_freq      = coh.freq;
         coh_connect   = coh.cohspctrm;
@@ -81,7 +50,6 @@ for j=1:length(file_list)
         save([path, strrep(file, '_FINISH', name_coh)],'coh')
         save([path, strrep(file, '_FINISH', name_freq)],'coh_freq')
         save([path, strrep(file, '_FINISH', name_tensor)],'coh_connect')
-        % save([path, file(1:8), '_CONECT_COHM.mat'],'cohm')
         
         save([correct_path, strrep(file, '_FINISH', name_freq)],'coh_connect') % MAS IMPORTANTE PARA TRABAJAR
     
